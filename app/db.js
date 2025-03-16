c2bef5bf2e9acfb9685f11ff2571ea466bfbab70
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-03-16 16:53:58 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
+        await mongoose.connect(`${process.env.MONGO_URL}/Users_Data`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         });
