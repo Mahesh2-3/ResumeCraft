f7e4faaf2fb2d644a3d2d13bd9a547c545158904
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-03-18 18:45:21 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-    <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+    <link rel="icon" type="image/jpeg" href="/resumecraft.jpg" />
       <body
         className="antialiased text-white "
       >
